009fa105b21655a64dfdc4c36398907cbd33c3d9
imbenrabi <imbenrabi@gmail.com>
2023-09-29 18:51:04 +0300

    add(server): elysia implementation

--- packages/server/server.ts
+++
@@ -1,8 +0,0 @@
-const server = Bun.serve({
-    port: 3333,
-    fetch(request) {
-      return new Response("Welcome to Bun!");
-    },
-  });
-  
-  console.log(`Listening on localhost: ${server.port}`);
